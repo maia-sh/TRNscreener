6e5d1640fb8ee09c718032a7b071ab564fdd9c2e
Benjamin G Carlisle <bgcarlisle@users.noreply.github.com>
2020-08-20 12:45:37 +0200

    Downloads data from ClinicalTrials.gov in batches of 100 NCT's

--- 8_run_trial_identifier_search.R
+++ 8_run_trial_identifier_search.R
@@ -1,115 +1,157 @@
+library(tidyverse)
 library(xml2)
 
 run_trial_identifier_search <- function(folder, save_file) {
     
-    # The following will print out a CSV with one row per
-    # clinical trial identifier found.
-    
-    # The first column of the output CSV contains the DOI
-    # of the paper in which the identifier was found.
-    
-    # The second column indicates the type of identifier
-    # e.g. NCT or ISRCTN. (More may be added)
-    
-    # The third column contains the identifier that was
-    # found
-    
-    # The fourth column indicates whether the NCT number
-    # corresponds to a legitimate record on
-    # clinicaltrials.gov. 1 means yes, 0 means no, and
-    # NA means that it is not an NCT number.
-    
-    # NOTE: The script checks whether an NCT number is
-    # legit by connecting to the ClinicalTrials.gov API
-    # so you will need an internet connexion for this to
-    # work.
-    
-    # If you want to add new identifiers to be searched
-    # Add them to this list
-    identifiers <- list(
-        c("NCT", "NCT[0-9 -]+[0-9]"),
-        c("ISRCTN", "ISRCTN[0-9 -]+[0-9]")
-    )
-    # End of list of identifiers to search for
-    
-    print("Identifying trials ...")
-    
-    text_list <- paste0(folder, list.files(folder))
-    
-    new_row <- data.frame("doi", "type", "identifier", "clinicaltrials.gov")
-    write_csv(new_row, save_file, append=TRUE)
-    
-    for ( identifier in identifiers ) {
+    if ( ! file.exists(save_file) ) {
         
-        print(paste("Searching for", identifier[1], "identifiers"))
+        # The following will print out a CSV with one row per
+        # clinical trial identifier found.
         
-        for(filename in text_list) {
+        # The first column of the output CSV contains the DOI
+        # of the paper in which the identifier was found.
+        
+        # The second column indicates the type of identifier
+        # e.g. NCT or ISRCTN. (More may be added)
+        
+        # The third column contains the identifier that was
+        # found
+        
+        # The fourth column indicates whether the NCT number
+        # corresponds to a legitimate record on
+        # clinicaltrials.gov. 1 means yes, 0 means no, and
+        # NA means that it is not an NCT number.
+        
+        # NOTE: The script checks whether an NCT number is
+        # legit by connecting to the ClinicalTrials.gov API
+        # so you will need an internet connexion for this to
+        # work.
+        
+        # If you want to add new identifiers to be searched
+        # Add them to this list
+        identifiers <- list(
+            c("NCT", "NCT[0-9 -]+[0-9]"),
+            c("ISRCTN", "ISRCTN[0-9 -]+[0-9]")
+        )
+        # End of list of identifiers to search for
+        
+        print("Identifying trials ...")
+        
+        text_list <- paste0(folder, list.files(folder))
+        
+        new_row <- data.frame("doi", "type", "identifier")
+        write_csv(new_row, save_file, append=TRUE)
+        
+        for ( identifier in identifiers ) {
             
-            file_doi <- str_replace(
-                str_replace(
-                    strsplit(filename, "/")[[1]][length(strsplit(filename, "/")[[1]])],
-                    ".txt", ""
-                ),
-                "\\+", "\\/"
-            )
+            print(paste("Searching for", identifier[1], "identifiers"))
             
-            text <- readChar(filename, file.info(filename)$size)
-            
-            idmatches <- regmatches(text, gregexpr(identifier[2], text))
-            
-            if (length(idmatches[[1]]) > 0) {
+            for(filename in text_list) {
                 
-                # Remove the spaces and hyphens from matches
+                file_doi <- str_replace(
+                    str_replace(
+                        strsplit(filename, "/")[[1]][length(strsplit(filename, "/")[[1]])],
+                        ".txt", ""
+                    ),
+                    "\\+", "\\/"
+                )
                 
-                cleaned_matches <- c()
+                text <- readChar(filename, file.info(filename)$size)
                 
-                print(file_doi)
+                idmatches <- regmatches(text, gregexpr(identifier[2], text))
                 
-                for (idmatch in idmatches[[1]]) {
+                if (length(idmatches[[1]]) > 0) {
                     
-                    idmatch <- str_remove_all(idmatch, " ")
-                    idmatch <- str_remove_all(idmatch, "-")
+                    # Remove the spaces and hyphens from matches
                     
-                    cleaned_matches <- c(cleaned_matches, idmatch)
+                    cleaned_matches <- c()
                     
-                }
-                
-                cleaned_matches <- unique(cleaned_matches)
-                
-                if ( identifier[1] == "NCT") { # If it's an NCT number, check that it exists on CT dot gov
+                    print(file_doi)
+                    
+                    for (idmatch in idmatches[[1]]) {
+                        
+                        idmatch <- str_remove_all(idmatch, " ")
+                        idmatch <- str_remove_all(idmatch, "-")
+                        
+                        cleaned_matches <- c(cleaned_matches, idmatch)
+                        
+                    }
+                    
+                    cleaned_matches <- unique(cleaned_matches)
                     
                     for (idmatch in cleaned_matches) {
-                        
-                        temp <- tempfile()
-                        
-                        download.file(
-                            paste0("https://clinicaltrials.gov/api/query/full_studies?expr=", idmatch),
-                            temp
-                        )
-                        
-                        xml <- read_xml(temp)
-                        
-                        unlink(temp)
-                        
-                        if (xml_text(xml_find_all(xml, "/FullStudiesResponse/NStudiesFound")) != "0") {
-                            new_row <- data.frame(file_doi, identifier[1], idmatch, 1)
-                        } else {
-                            new_row <- data.frame(file_doi, identifier[1], idmatch, 0)
-                        }
+                        new_row <- data.frame(file_doi, identifier[1], idmatch)
                         write_csv(new_row, save_file, append=TRUE)
                     }
                     
-                } else { # It's not an NCT number that we're looking at, so we won't check for it on CT dot gov
-                    for (idmatch in cleaned_matches) {
-                        new_row <- data.frame(file_doi, identifier[1], idmatch, "NA")
-                        write_csv(new_row, save_file, append=TRUE)
-                    }
                 }
                 
             }
             
         }
         
+        # At this point, all the Identifiers have been written to a CSV.
+        # Now, we check the NCT's for whether they're on CT dot gov.
+        
+        # The batch size must be 100 or fewer.
+        batch_size <- 100
+        
+        found_ncts <- read_csv(save_file) %>%
+            filter(type == "NCT")
+        found_ncts$doi <- NULL
+        found_ncts$type <- NULL
+        found_ncts$`clinicaltrials.gov` <- ""
+        
+        while ( nrow(filter(found_ncts, `clinicaltrials.gov` == "")) > 0 ) {
+            
+            ncts_to_check <- found_ncts %>%
+                filter(`clinicaltrials.gov` == "") %>%
+                select(identifier) %>%
+                head(n=batch_size)
+            
+            temp <- tempfile()
+            
+            download.file(
+                paste0(
+                    "https://clinicaltrials.gov/api/query/full_studies?min_rnk=1&max_rnk=",
+                    batch_size,
+                    "&expr=",
+                    URLencode(
+                        paste(ncts_to_check$identifier, collapse = " OR ")
+                    )
+                ),
+                temp
+            )
+            
+            xml <- read_xml(temp)
+            
+            unlink(temp)
+            
+            dl_trials <- xml_text(xml_find_all(xml, "/FullStudiesResponse/FullStudyList/FullStudy/Struct[contains(@Name, 'Study')]/Struct[contains(@Name, 'ProtocolSection')]/Struct[contains(@Name, 'IdentificationModule')]/Field[contains(@Name, 'NCTId')]"))
+            
+            for (nct in ncts_to_check$identifier) {
+                
+                found_ncts[found_ncts$identifier == nct, "clinicaltrials.gov"] <- as.character(nct %in% dl_trials)
+                
+            }
+            
+        }
+        
+        # Now found_ncts contains all the unique NCT numbers and whether
+        # they correspond to a registry entry on clinicaltrials.gov.
+        # The following joins these results to the previously saved CSV
+        # and over-writes the CSV.
+        
+        found_identifiers <- read_csv(save_file) %>%
+            left_join(found_ncts, by = "identifier")
+        
+        write_csv(found_identifiers, save_file, append=FALSE)
+        
+    } else { # Print out an error because the save_file already exists
+        
+        print (paste("Error:", save_file, "already exists"))
     }
     
 }
+
+run_trial_identifier_search("papers-2020-08-19/", "test-2020-08-20.csv")
